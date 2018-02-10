1c23f8cffe9f2879580349dea44c25a43c23461f
awalakaushik <kawala@uh.edu>
2018-02-10 06:21:46 -0600

    Add MAP files

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// Foursquare API credentials
+var ClientID = "XIAQLFODY15IXHPQQT3O5APHDV1FWW1ILULATZG12DPS2ZDM";
+var ClientSecret = "NNI2YCIOKFDSIUXKIBKQK1MJ2B4TLI3UHJ3M2PCEKPGN53LN";
+
+var AppViewModel = function() {
+	var self = this;
+	console.log("AppViewModel Load");
+};
---
+++ js/map.js
@@ -0,0 +1,16 @@
+var map;
+function initMap() {
+	// Constructor creates a new map - only center and zoom are required.
+	map = new google.maps.Map(document.getElementById('map'), {
+	  center: {lat: 40.7413549, lng: -73.9980244},
+	  zoom: 13
+	});
+	console.log("map loads successfully");
+	// Apply Knockout Bindings so that the list gets populated after the map loads
+	//ko.ApplyBindings(new AppViewModel());
+}
+
+// function to handle error while loading the map
+function handleMapErrorOnLoad() {
+
+}
